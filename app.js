1d6067dae91ca9a429a9ac85a93b4d8bc9897586
Higor Oliveira <higorsouz@gmail.com>
2023-07-25 18:55:51 -0300

    adicianando um console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento e jogos");
+console.log("aplicação está se conectando ao banco de dados ")
+// mais um console logo teste:
+console.log("Enviando para o GitHub")

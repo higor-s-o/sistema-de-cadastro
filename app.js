1bc4816ff59664d64a7c94656e2a194c5316339f
Higor Oliveira <higorsouz@gmail.com>
2023-07-25 19:10:45 -0300

    voltando para a versão 05de54b

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("Rodando o sistema de gerenciamento e jogos");
-console.log("aplicação está se conectando ao banco de dados ")
-// mais um console logo teste:
-console.log("Enviando para o GitHub")
